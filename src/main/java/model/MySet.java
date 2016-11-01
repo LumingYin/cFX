e2e9df66c781755c44262623372daf157ac58fbd
Bright <Bright@lawn-143-215-55-237.lawn.gatech.edu>
2016-10-31 22:25:34 -0400

    Removes debug code and generic cleanup

--- src/main/java/model/MySet.java
+++ src/main/java/model/MySet.java
@@ -1,5 +1,6 @@
 package model;
 import java.util.Random;
+@SuppressWarnings("unchecked")
 
 class MySet<E> implements SimpleSet<E> {
 
@@ -13,20 +14,6 @@ class MySet<E> implements SimpleSet<E> {
         hasNullInSet = false;
         array = (E[]) new Object[1];
         this.size = 0;
-    }
-
-    // Debug SOPs
-    public void debug() {
-        System.out.println();
-        System.out.println("Printing debug info:");
-        System.out.println("Private Array Length: " + array.length);
-        System.out.println("Private Size Variable: " + size);
-        System.out.println("Public size() returns: " + size());
-        System.out.println("hasNullInSet: " + hasNullInSet);
-        for (int  i = 0; i < array.length; i++) {
-            System.out.println("[" + i + "]: " + array[i]);
-        }
-        System.out.println();
     }
 
     // These are private methods to help with the expansion and sorting of array
@@ -74,13 +61,6 @@ class MySet<E> implements SimpleSet<E> {
         }
     }
 
-    /**
-     * Removes the passed in element from the set if it is
-     * contained in the set, then returns the removed element
-     * otherwise throws as ElementDoesNotExistException
-     * @param e the element to be removed
-     * @return E the element that was removed
-     */
     public E remove(E e) throws ElementDoesNotExistException {
         E passedInElement = e;
         if (passedInElement == null) {
@@ -107,15 +87,6 @@ class MySet<E> implements SimpleSet<E> {
         return passedInElement;
     }
 
-    /**
-     * Returns true if the passed in element is contained in
-     * the set.
-     * Returns false if the passed in element is not contained
-     * in the set.
-     * @param e the element to be checked
-     * @return whether the passed in element is contained
-     * in the set
-     */
     public boolean contains(E e) {
         if (e == null) {
             if (hasNullInSet) {
@@ -131,15 +102,6 @@ class MySet<E> implements SimpleSet<E> {
         return false;
     }
 
-    /**
-     * removes all the passed in elements from the set then returns them
-     * throws a ElementDoesNotExistException if one of the passed in elements
-     * is not in the set.
-     * NOTE: this method should not remove ANY elements from the set
-     * if it throws a ElementDoesNotExistException
-     * @param e the elements to be removed from the set
-     * @return E[] the elements that were removed from the set
-     */
     public E[] removeAll(E[] e) throws ElementDoesNotExistException {
         for (E content:e) {
             if (!contains(content)) {
@@ -155,9 +117,6 @@ class MySet<E> implements SimpleSet<E> {
         return e;
     }
 
-    /**
-     * Removes all elements from the set.
-     */
     public void clear() {
         for (int i = 0; i < array.length; i++) {
             array[i] = null;
@@ -166,10 +125,6 @@ class MySet<E> implements SimpleSet<E> {
         size = 0;
     }
 
-    /**
-     * Returns the number of elements contained in the set.
-     * @return int the number of elements in the set
-     */
     public int size() {
         sortArray();
         if (hasNullInSet) {
@@ -192,16 +147,6 @@ class MySet<E> implements SimpleSet<E> {
         return isEmpty;
     }
 
-    /**
-     * Returns a random element from the set.
-     * NOTE: because the set has no ordering returning any element in the
-     * set is effectively returning a "random" element
-     * throws a ElementDoesNotExistException if there are no elements
-     * in the set
-     * HINT: you may import java.util.Random to help with this if
-     * you want
-     * @return E a random element from the set
-     */
     public E getRandomElement() throws ElementDoesNotExistException {
         // sortArray();
         if (isEmpty()) {
@@ -221,12 +166,6 @@ class MySet<E> implements SimpleSet<E> {
         return array[randomNumber];
     }
 
-    /**
-     * Returns an array of all the elements contained within the SimpleSet.
-     * There should be no nulls in the array.
-     * @return an array containing all the elements contained
-     * within the set
-     */
     public E[] toArray() {
         sortArray();
         E[] toBeReturned;
@@ -241,12 +180,6 @@ class MySet<E> implements SimpleSet<E> {
         return toBeReturned;
     }
 
-    /**
-     * Calculates and returns a String representation of this object.
-     * Any String that shows each individual element's toString
-     * is acceptable.
-     * @return String the String representation of this object
-     */
     @Override
     public String toString() {
         String compositedString = "";
